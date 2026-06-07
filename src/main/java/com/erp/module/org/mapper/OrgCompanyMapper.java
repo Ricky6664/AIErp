9378d5addefaae49ab24c52e3b66eb5fbf7e2299
Ricky6664 <Ricky6664@users.noreply.github.com>
2026-06-08 01:05:31 +0800

    feat(P0-006): 编写OrgCompanyServiceImpl实现类
    - page/keyword搜索+enabled筛选, create/名称唯一性+信用代码校验
    - update/乐观锁+排除自身, delete/关联部门检查+逻辑删除
    - 新增OrgCompanyMapper+OrgDepartmentMapper+OrgDepartment实体stub
    
    Task: P0-006-001-001-001-002

---
+++ src/main/java/com/erp/module/org/mapper/OrgCompanyMapper.java
@@ -0,0 +1,9 @@
+package com.erp.module.org.mapper;
+
+import com.erp.common.mapper.BaseMapperX;
+import com.erp.module.org.entity.OrgCompany;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrgCompanyMapper extends BaseMapperX<OrgCompany> {
+}
